4d74db2a3fa662b3990bbd9fcd370bfa69094c11
Andraz Pergar <andraz.pergar10@gmail.com>
2021-02-13 18:46:31 +0100

    Added bootstrap

--- react/src/index.js
+++ react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
